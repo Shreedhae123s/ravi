fae62f978336521a4bcf0e22406d9da098e61312
Muttu <itsmemutturaj@gmail.com>
2024-07-18 23:12:02 -0700

    mugya

---
+++ Oddeven.java
@@ -0,0 +1,21 @@
+package ananya;
+import java.util.Scanner; 
+class Oddeven
+{
+	  public static void main(String[]args)
+	  {
+		  int n;
+		  Scanner s=new Scanner(System.in);
+		  System.out.println("Enter then number you want to check:");
+		  n=s.nextInt();
+		  if(n%2==0)
+		  {
+			  System.out.println("The given number"+n+" is Even");
+		  }
+		  else
+		  {
+			  System.out.println("The given number"+n+" is add");
+     }			 
+    }
+   }	
+
